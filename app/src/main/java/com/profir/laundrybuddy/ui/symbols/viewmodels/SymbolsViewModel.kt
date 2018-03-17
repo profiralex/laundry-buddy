a77fd94c024bfbd19742b74e4f9c0e807cfaac53
Profir Alexandr <profiralex@gmail.com>
2018-03-17 22:54:00 +0200

    Add dry symbols.

--- app/src/main/java/com/profir/laundrybuddy/ui/symbols/viewmodels/SymbolsViewModel.kt
+++ app/src/main/java/com/profir/laundrybuddy/ui/symbols/viewmodels/SymbolsViewModel.kt
@@ -15,11 +15,12 @@ class SymbolsViewModel : ViewModel() {
 
     fun loadSymbols() {
         _symbols.value = listOf(
+                // Wash
                 Symbol(R.string.wash_normal_title, R.string.wash_normal_description, R.drawable.ic_wash_normal),
+                Symbol(R.string.wash_permanent_press_title, R.string.wash_permanent_press_description, R.drawable.ic_wash_permanent_press),
                 Symbol(R.string.wash_delicate_title, R.string.wash_delicate_description, R.drawable.ic_wash_delicate),
                 Symbol(R.string.wash_hand_title, R.string.wash_hand_description, R.drawable.ic_wash_hand),
                 Symbol(R.string.wash_no_title, R.string.wash_no_description, R.drawable.ic_wash_no),
-                Symbol(R.string.wash_permanent_press_title, R.string.wash_permanent_press_description, R.drawable.ic_wash_permanent_press),
                 Symbol(R.string.wash_temperature_30_celsius_title, R.string.wash_temperature_30_celsius_description, R.drawable.ic_wash_temperature_30_celsius),
                 Symbol(R.string.wash_temperature_30_title, R.string.wash_temperature_30_description, R.drawable.ic_wash_temperature_30),
                 Symbol(R.string.wash_temperature_40_celsius_title, R.string.wash_temperature_40_celsius_description, R.drawable.ic_wash_temperature_40_celsius),
@@ -31,7 +32,27 @@ class SymbolsViewModel : ViewModel() {
                 Symbol(R.string.wash_temperature_70_celsius_title, R.string.wash_temperature_70_celsius_description, R.drawable.ic_wash_temperature_70_celsius),
                 Symbol(R.string.wash_temperature_70_title, R.string.wash_temperature_70_description, R.drawable.ic_wash_temperature_70),
                 Symbol(R.string.wash_temperature_95_celsius_title, R.string.wash_temperature_95_celsius_description, R.drawable.ic_wash_temperature_95_celsius),
-                Symbol(R.string.wash_temperature_95_title, R.string.wash_temperature_95_description, R.drawable.ic_wash_temperature_95)
+                Symbol(R.string.wash_temperature_95_title, R.string.wash_temperature_95_description, R.drawable.ic_wash_temperature_95),
+
+                // Tumble dry
+                Symbol(R.string.tumble_dry_normal_title, R.string.tumble_dry_normal_description, R.drawable.ic_tumble_dry_normal),
+                Symbol(R.string.tumble_dry_heat_low_title, R.string.tumble_dry_heat_low_description, R.drawable.ic_tumble_dry_heat_low),
+                Symbol(R.string.tumble_dry_heat_medium_title, R.string.tumble_dry_heat_medium_description, R.drawable.ic_tumble_dry_heat_medium),
+                Symbol(R.string.tumble_dry_heat_high_title, R.string.tumble_dry_heat_high_description, R.drawable.ic_tumble_dry_heat_high),
+                Symbol(R.string.tumble_dry_heat_no_title, R.string.tumble_dry_heat_no_description, R.drawable.ic_tumble_dry_heat_no),
+                Symbol(R.string.tumble_dry_permanent_press_title, R.string.tumble_dry_permanent_press_description, R.drawable.ic_tumble_dry_permanent_press),
+                Symbol(R.string.tumble_dry_gentle_title, R.string.tumble_dry_gentle_description, R.drawable.ic_tumble_dry_gentle),
+                Symbol(R.string.tumble_dry_no_title, R.string.tumble_dry_no_description, R.drawable.ic_tumble_dry_not),
+
+                // Dry
+                Symbol(R.string.dry_no_title, R.string.dry_no_description, R.drawable.ic_dry_no),
+                Symbol(R.string.dry_line_title, R.string.dry_line_description, R.drawable.ic_dry_line),
+                Symbol(R.string.dry_drip_title, R.string.dry_drip_description, R.drawable.ic_dry_drip),
+                Symbol(R.string.dry_flat_title, R.string.dry_flat_description, R.drawable.ic_dry_flat),
+                Symbol(R.string.dry_shade_title, R.string.dry_shade_description, R.drawable.ic_dry_shade),
+
+                // Wring
+                Symbol(R.string.dry_wring_no_title, R.string.dry_wring_no_description, R.drawable.ic_dry_wring_no)
         )
     }
 }
